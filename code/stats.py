d04345a55693d5089c213e022cbd1cc2ba50f081
Edward Lu <edward.k.lu@harbinger.ischool.berkeley.edu>
2013-02-12 17:36:14 -0800

    edited stats.py to handle all stats including z-score and stats per candidate. Added financial data for CA presidential campaign.

--- code/stats.py
+++ code/stats.py
@@ -5,43 +5,99 @@ available from http://www.fec.gov/disclosurep/PDownload.do"""
 import fileinput
 import csv
 
-total = 0
+## Data read
+candidates = set()
+cMap = {}
+cMap['aggregate'] = {'contribs':[]}
 
 for row in csv.reader(fileinput.input()):
     if not fileinput.isfirstline():
-        total += float(row[9])
-        ###
-        # TODO: calculate other statistics here
-        # You may need to store numbers in an array to access them together
-        ##/
+        candidate = row[2]
+        contrib = float(row[9])
+        candidates.add(candidate)
+        if (candidate not in cMap):
+            cMap[candidate] = {'contribs':[]}
+        cMap[candidate]['contribs'].append(contrib)
+        cMap['aggregate']['contribs'].append(contrib)
 
-###
-# TODO: aggregate any stored numbers here
-#
-##/
+numContributions = len(cMap['aggregate']['contribs'])
 
-##### Print out the stats
-print "Total: %s" % total
-print "Minimum: "
-print "Maximum: "
-print "Mean: "
-print "Median: "
-# square root can be calculated with N**0.5
-print "Standard Deviation: "
+if (numContributions == 0):
+    print "No data received! Exiting..."
+    import sys
+    sys.exit(-1)
 
-##### Comma separated list of unique candidate names
-print "Candidates: "
+## Function definitions
 
-def minmax_normalize(value):
+def getAverage(array):
+    return sum(array)/float(len(array))
+
+def getStandardDev(array):
+    mean = getAverage(array)
+    return getAverage([(val - mean)**2 for val in array])**0.5
+
+def getMedian(array):
+    from math import floor
+    array = sorted(array)
+    midx = int(floor(len(array) / 2))
+    median = array[midx]
+    if (len(array) % 2 == 0):
+        median = (array[midx] + array[midx - 1])/ 2.0
+    return median
+
+def minmax_normalize(value, maximum, minimum):
     """Takes a donation amount and returns a normalized value between 0-1. The
     normilzation should use the min and max amounts from the full dataset"""
-    ###
-    # TODO: replace line below with the actual calculations
-    norm = value
-    ###/
-    
+    newMin = 0
+    newMax = 1
+    norm = ((value - minimum) / (maximum - minimum)) * (newMax - newMin) + newMin
     return norm
 
-##### Normalize some sample values
-print "Min-max normalized values: %r" % map(minmax_normalize, [2500, 50, 250, 35, 8, 100, 19])
+def zscore(value, mean, sd):
+    return (value - mean) / sd
+
+def printStats(stats, name):
+    total = stats['total']
+    minimum = stats['minimum']
+    maximum = stats['maximum']
+    mean = stats['mean']
+    median = stats['median']
+    sd = stats['sd']
+
+    print "\n" + "#"*20+"\nStats for: " + name +"\n"+"#"*20
+
+    ##### Print out the stats
+    print "Total: %s" % total
+    print "Minimum: %s" % minimum
+    print "Maximum: %s" % maximum
+    print "Mean: %s" % mean
+    print "Median: %s" % median
+    # square root can be calculated with N**0.5
+    print "Standard Deviation: %s" % sd
+
+    ##### Normalize some sample values
+    values = [2500, 50, 250, 35, 8, 100, 19]
+    print "Values before: %r" % values
+    print "Min-max normalized values: %r" % map(lambda val: minmax_normalize(val, maximum, minimum), values)
+    print "Z-score values: %r" % map(lambda val: zscore(val, mean, sd), values)
+
+##### Comma separated list of unique candidate names
+print "Candidates: " + ', '.join(candidates)
+
+keys = cMap.keys()
+keys.remove('aggregate')
+
+for key in ['aggregate'] + keys:
+    stats = cMap[key]
+    contributions = stats['contribs']
+    ## Set variables
+    stats['total'] = sum(contributions)
+    stats['minimum'] = min(contributions)
+    stats['maximum'] = max(contributions)
+    stats['mean'] = getAverage(contributions)
+    stats['median'] = getMedian(contributions)
+    stats['sd'] = getStandardDev(contributions)
+    printStats(stats, key)
+
+
 
