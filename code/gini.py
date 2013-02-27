a4df05f1982d815c5c9ea12c3c48e3d4acdf5de3
Edward Lu <edward.k.lu@harbinger.ischool.berkeley.edu>
2013-02-26 22:10:40 -0800

    saving progress

--- code/gini.py
+++ code/gini.py
@@ -12,7 +12,10 @@ receipt_desc, memo_cd, memo_text, form_tp, file_num, tran_id, election_tp) = ran
 
 
 ############### Set up variables
-# TODO: declare datastructures
+
+total_entries = 0
+items_per_name = {}
+ipn_per_zip = {}
 
 ############### Read through files
 for row in csv.reader(fileinput.input()):
@@ -20,13 +23,33 @@ for row in csv.reader(fileinput.input()):
         ###
         # TODO: replace line below with steps to save information to calculate
         # Gini Index
-        row[cand_nm], row[contbr_zip]
+        name = row[cand_nm]
+        zipc = row[contbr_zip]
+        if zipc not in ipn_per_zip:
+            ipn_per_zip[zipc] = {'total':0, 'ipns': {}}
+        if name not in items_per_name:
+            items_per_name[name] = 0
+        if name not in ipn_per_zip[zipc]['ipns']:
+            ipn_per_zip[zipc]['ipns'][name] = 0
+        items_per_name[name] += 1
+        ipn_per_zip[zipc]['ipns'][name] += 1
+        total_entries += 1
         ##/
 
 ###
 # TODO: calculate the values below:
 gini = 0  # current Gini Index using candidate name as the class
+
+total_entries = float(total_entries)
+gini = 1 - sum([(v/total_entries)**2 for v in items_per_name.values()])
+
 split_gini = 0  # weighted average of the Gini Indexes using candidate names, split up by zip code
+
+for info in ipn_per_zip.values():
+    temp_total = float(info['total'])
+    temp_gini = 1 - sum([(ipn/temp_total)**2 for ipn in info['ipns'].values()])
+    split_gini += temp_gini * temp_total / total_entries
+
 ##/
 
 print "Gini Index: %s" % gini
